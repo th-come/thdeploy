845dccda12535d5ecc0b6ca9941a8e19576ed93c
tanghao <tanghao@youloft.com>
2022-03-28 15:36:41 +0800

    版本迭代

--- src/extension.ts
+++ src/extension.ts
@@ -5,11 +5,21 @@ import { DepNodeProvider, Dependency } from './nodeDependencies';
 // 激活事件
 export function activate(context: vscode.ExtensionContext) {
 	console.log("---------------组件激活---------------");
-	const nodeDependenciesProvider = new DepNodeProvider(vscode.workspace.rootPath || "");
+
+	if (vscode.workspace.workspaceFolders?.length === 0) {
+		return
+	}
+	
+	// @ts-ignore
+	const rootPath =  vscode.workspace.workspaceFolders[0]?.uri._fsPath
+	
+	// @ts-ignore
+	console.log('文件夹目录', rootPath)
+	console.log('旧版本目录', vscode.workspace.rootPath)
+	const nodeDependenciesProvider = new DepNodeProvider(rootPath || "");
 	vscode.window.registerTreeDataProvider('nodeDependencies', nodeDependenciesProvider);
 	vscode.commands.registerCommand('nodeDependencies.refreshEntry', () => nodeDependenciesProvider.refresh());
 	vscode.commands.registerCommand('nodeDependencies.uploadEntry', uploadEntry);
-	vscode.commands.registerCommand('nodeDependencies.deleteEntry', () => vscode.window.showInformationMessage('暂不支持删除'))
 }
 
 // 销毁周期
